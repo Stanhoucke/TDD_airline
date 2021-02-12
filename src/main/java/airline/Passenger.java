d5a9a0290f2ba044d3ab8459c4b733dbe5981ff7
Stanhoucke <stanhoucke@gmail.com>
2021-02-12 20:43:59 +0000

    passenger starts with flight as null

--- src/main/java/airline/Passenger.java
+++ src/main/java/airline/Passenger.java
@@ -3,14 +3,24 @@ package airline;
 public class Passenger {
     private String name;
     private int numberOfBags;
+    private Flight flight;
 
     public Passenger(String name, int numberOfBags) {
         this.name = name;
         this.numberOfBags = numberOfBags;
+        this.flight = null;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public Flight getFlight() {
+        return this.flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
     }
 
     public int getNumberOfBags() {
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,16 +1,33 @@
+import airline.Airport;
+import airline.Flight;
 import airline.Passenger;
 import org.junit.Before;
 import org.junit.Test;
+import staff.CabinCrewMember;
+import staff.Pilot;
+import staff.RankType;
+import vehicle.Plane;
+import vehicle.PlaneType;
+
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class PassengerTest {
 
     Passenger passenger1;
+    Pilot pilot1;
+    Plane plane1;
+    Flight flight1;
 
     @Before
     public void before(){
-        passenger1 = new Passenger("Neville Flynn", 2);
+        pilot1 = new Pilot("Sully", RankType.CAPTAIN, "JA-5678");
+        plane1 = new Plane(PlaneType.A380);
+        passenger1 = new Passenger("Neville Flynn", 0);
+
+        flight1 = new Flight(pilot1, plane1, "JAA6902", Airport.EDI, Airport.GVA, LocalDateTime.of(2021, 2, 15, 8, 30));
     }
 
     @Test
@@ -20,6 +37,11 @@ public class PassengerTest {
 
     @Test
     public void hasNumberOfBags(){
-        assertEquals(2, passenger1.getNumberOfBags());
+        assertEquals(0, passenger1.getNumberOfBags());
+    }
+
+    @Test
+    public void passengerStartsWithNoFlight(){
+        assertNull(passenger1.getFlight());
     }
 }
