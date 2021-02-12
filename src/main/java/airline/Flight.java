df8137ed767d98b7650db7d8abf92d32aee2e20d
Stanhoucke <stanhoucke@gmail.com>
2021-02-12 15:24:58 +0000

    flight passes attribute tests

--- src/main/java/airline/Flight.java
+++ src/main/java/airline/Flight.java
@@ -1,4 +1,62 @@
 package airline;
 
+import staff.CabinCrewMember;
+import staff.Pilot;
+import vehicle.Plane;
+
+import java.util.ArrayList;
+
 public class Flight {
+    private Pilot pilot;
+    private ArrayList<CabinCrewMember> crew;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private Airport destination;
+    private Airport departureAirport;
+    private String departureTime;
+
+    public Flight(Pilot pilot, Plane plane, String flightNumber, Airport destination, Airport departureAirport, String departureTime) {
+        this.pilot = pilot;
+        this.crew = new ArrayList<CabinCrewMember>();
+        this.passengers = new ArrayList<Passenger>();
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+    // Getters
+    public Pilot getPilot() {
+        return this.pilot;
+    }
+
+    public ArrayList<CabinCrewMember> getCrew() {
+        return this.crew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return this.passengers;
+    }
+
+    public Plane getPlane() {
+        return this.plane;
+    }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
+
+    public Airport getDestination() {
+        return this.destination;
+    }
+
+    public Airport getDepartureAirport() {
+        return this.departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return this.departureTime;
+    }
 }
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,70 @@
+import airline.Airport;
+import airline.Flight;
+import airline.Passenger;
+import org.junit.Before;
+import org.junit.Test;
+import staff.CabinCrewMember;
+import staff.Pilot;
+import staff.RankType;
+import vehicle.Plane;
+import vehicle.PlaneType;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightTest {
+
+    Flight flight1;
+    Pilot pilot1;
+    Plane plane1;
+
+    @Before
+    public void before(){
+        pilot1 = new Pilot("Sully", RankType.CAPTAIN, "JA-5678");
+        plane1 = new Plane(PlaneType.A380);
+
+        flight1 = new Flight(pilot1, plane1, "JAA6902", Airport.EDI, Airport.GVA, "2021-02-15 08:00");
+    }
+
+    // Attribute tests
+    @Test
+    public void hasPilot() {
+        assertEquals(pilot1, flight1.getPilot());
+    }
+
+    @Test
+    public void hasCrew(){
+        assertEquals(new ArrayList<CabinCrewMember>(), flight1.getCrew());
+    }
+
+    @Test
+    public void hasPassengers(){
+        assertEquals(new ArrayList<Passenger>(), flight1.getPassengers());
+    }
+
+    @Test
+    public void hasPlane(){
+        assertEquals(plane1, flight1.getPlane());
+    }
+
+    @Test
+    public void hasFlightNumber(){
+        assertEquals("JAA6902", flight1.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals(Airport.EDI, flight1.getDestination());
+    }
+
+    @Test
+    public void hasDepartureAirport(){
+        assertEquals(Airport.GVA, flight1.getDepartureAirport());
+    }
+
+    @Test
+    public void hasDepartureTime(){
+        assertEquals("2021-02-15 08:00", flight1.getDepartureTime());
+    }
+}
